3075c1bf6a58e7910d4360e7b09ca9e30c148fb8
Michael Salisbury <m.slzbry@gmail.com>
2023-08-27 13:00:56 -0700

    set up initial scaffolding

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+// Set up connection for mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Turn debug to true so we can log queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected to Moongoose at localhost:${PORT}`));
